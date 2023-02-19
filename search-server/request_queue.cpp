26882f97cf1d87065a88080fbefbf37460490d56
zaalexey <119343901+zaalexey@users.noreply.github.com>
2023-02-19 20:21:54 +0500

    Update request_queue.cpp

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -1,32 +1,32 @@
 #include "request_queue.h"
 
-    RequestQueue::RequestQueue(const SearchServer& search_server)
-        :search_server_(search_server) {}
+RequestQueue::RequestQueue(const SearchServer& search_server)
+    :search_server_(search_server) {}
 
-    vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentStatus status) {
-        auto result = search_server_.FindTopDocuments(raw_query, status);
-        CheckRequests(result, raw_query);
-        return result;
-    }
-    vector<Document> RequestQueue::AddFindRequest(const string& raw_query) {
-        auto result = search_server_.FindTopDocuments(raw_query);
-        CheckRequests(result, raw_query);
-        return result;
-    }
-    int RequestQueue::GetNoResultRequests() const {
-        return empty_requests_;
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentStatus status) {
+    auto result = search_server_.FindTopDocuments(raw_query, status);
+    CheckRequests(result, raw_query);
+    return result;
+}
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query) {
+    auto result = search_server_.FindTopDocuments(raw_query);
+    CheckRequests(result, raw_query);
+    return result;
+}
+int RequestQueue::GetNoResultRequests() const {
+    return empty_requests_;
+}
+
+void RequestQueue::CheckRequests(vector<Document> result, const string& raw_query) {
+    requests_.push_back({ raw_query, !result.empty() });
+
+    if (!requests_.back().is_result) {
+        empty_requests_++;
     }
 
-    void RequestQueue::CheckRequests(vector<Document> result, const string& raw_query) {
-        requests_.push_back({ raw_query, !result.empty() });
-
-        if (!requests_.back().is_result) {
-            empty_requests_++;
-        }
-
-        if (requests_.size() - 1 == min_in_day_) {
-            if (!requests_.front().is_result)
-                empty_requests_--;
-            requests_.pop_front();
-        }
+    if (requests_.size() - 1 == min_in_day_) {
+        if (!requests_.front().is_result)
+            empty_requests_--;
+        requests_.pop_front();
     }
+}
